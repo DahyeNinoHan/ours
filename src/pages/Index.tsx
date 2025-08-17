4062c565884c5401ee752f3d43046911dfb08997
DahyeNinoHan <dahye.nino.han@gmail.com>
2025-08-17 08:35:53 -0400

    logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,6 +11,8 @@ const Index = () => {
 
   const handleSplashClick = () => {
     setShowSplash(false);
+    // 캐릭터 선택 페이지로 바로 이동
+    // 현재 구조상 splash가 사라지면 바로 캐릭터 선택이 나오므로, splash에서 바로 navigate할 수도 있음
   };
 
   const handleCharacterComplete = (newCharacter: Character) => {
@@ -31,6 +33,10 @@ const Index = () => {
           alignItems: "center",
           justifyContent: "center",
           zIndex: 9999,
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          setShowSplash(false);
         }}
       >
         <img
