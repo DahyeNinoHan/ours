afe80537089670f0242f73b69e169ef5ad31bc56
DahyeNinoHan <dahye.nino.han@gmail.com>
2025-09-04 02:38:54 -0400

    change

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -16,9 +16,6 @@ const Landing = () => {
         width: "100vw",
         height: "100vh",
         background: "#000000",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
         zIndex: 9999,
         cursor: "pointer",
       }}
@@ -28,6 +25,14 @@ const Landing = () => {
         src="/ours_logo_02.svg"
         alt="Ours Logo"
         className="w-[120vw] h-auto max-w-none max-h-none md:w-auto md:max-w-[60vw] md:max-h-[60vh]"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: "120vw",
+          height: "auto"
+        }}
         draggable={false}
       />
     </div>
